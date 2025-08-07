25ad5cdbee67a10e082dd964ae75a17b9e6e3441
Iskandar <akhund@desy.de>
2025-08-07 15:35:47 +0200

    Dictionary implementation (alpha)

--- fsspec_test.py
+++ fsspec_test.py
@@ -7,35 +7,38 @@ doi = "10.60717/041caef8-645a-4dd8-b12d-892ee03084c2"
 headers = {"Accept": "application/metalink4+xml"}
 response = requests.get(f"https://doi.org/{doi}", headers = headers, allow_redirects=True)
 
-if "metalink" in response.headers.get("Content-Type", ""):
+if "application/metalink4+xml" in response.headers.get("Content-Type", ""):
     root = ET.fromstring(response.text)
     ET.indent(root, space="  ", level=0)
     xml_string_pretty = ET.tostring(root, encoding="utf-8").decode("utf-8")
-
-    with open("output.txt", "w", encoding="utf-8") as f:
-        f.write(xml_string_pretty)
 
     ns = {'ml':root.tag.split("}")[0].strip("{")}
     urls = [url.text for url in root.findall(".//ml:url", namespaces=ns)]
     filenames = [file_elem.attrib['name'] for file_elem in root.findall(".//ml:file", namespaces=ns)]
     
-    names = []
-    i = 0
-    for filename in filenames:
-        names.append(filename)
 
-    url_dict = {name: None for name in names}
-
+    directory = {}
     i = 0
-    for url in urls:
-        url_dict[filenames[i]] = {'address': url}
+    for file in filenames:
+        before_slash = ""
+        for j, char in enumerate(filenames[i]):
+            if char == "/":
+                directory[before_slash] = {}
+                before_slash = ""
+            before_slash += char
+            
+            if j == len(filenames[i]) - 1:
+                for x in range(j, -1, -1):
+                    if filenames[i][j] == '/':
+                        break
+                    collected += filenames[i][j]
+              
+
+
+        
         i += 1
-       # with fsspec.open(url, mode='rb') as f:
-        #    content = f.read()
-         #   print(f"Downloaded {len(content)} bytes from {url}")
-
-    for key in url_dict.keys():
-        print(key)
+    
+    print(directory)
 
 else:
     print("No Metalink available")
